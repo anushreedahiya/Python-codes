bd8ae86e9d8c848e82f190f585e572295769e747
Anushree Dahiya <136242414+anushreedahiya@users.noreply.github.com>
2024-08-18 10:08:07 +0530

    python

--- factorial.py
+++ factorial.py
@@ -9,15 +9,3 @@ else:
         factorial=factorial*i
     print("factorial of ",n,"is", factorial)
 
-
-
-#using function
-def factorial(i):
-    if i==1:
-        return 1
-    else:
-        return (i*factorial(i-1))
-    
-number=int(input("enter the number="))
-print("The factorial of", number, "is", factorial(number))
-
